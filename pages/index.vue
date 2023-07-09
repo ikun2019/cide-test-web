40b73116957a8017e4c54a344a248f1a680b6e95
ikun <0215.miyatto@gmail.com>
2023-07-09 23:25:07 +0900

    create todo

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,51 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Todo</h1>
+    <div>
+      <input type="text" name="todo" id="todo" v-model="todo">
+      <button type="submit" @click.prevent="createTodo">投稿</button>
+    </div>
+    <div>
+      <ul v-for="(todo, index) in todos" :key="index">
+        <li>{{ todo.title }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data() {
+    return {
+      todo: '',
+      todos: []
+    }
+  },
+  mounted() {
+    this.fetchTodos()
+  },
+  methods: {
+    async fetchTodos() {
+      try {
+        const response = await this.$axios.$get(`${process.env.API_URL}/api`);
+        this.todos = response.todos;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async createTodo() {
+      try {
+        const data = {
+          todo: this.todo
+        };
+        const response = await this.$axios.$post(`${process.env.API_URL}/api`, data);
+        this.todo = '';
+        this.fetchTodos();
+        this.$router.push('/');
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 }
 </script>
